84a03b065ac8291a9a73c47b6e387dabe5c49f66
Ali-s1379 <ali_s1379@outlook.com>
2022-07-17 18:25:52 +0430

    Initial commit

---
+++ Assets/Player_mov.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_mov : MonoBehaviour
+{
+    Vector3 offset;
+    public Camera Gholam;
+    public float MovementSpeed;
+    
+    // Use this for initialization
+    void Start()
+    {
+        offset = Gholam.transform.position - transform.position;
+        
+        MovementSpeed = 5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += transform.forward * Time.deltaTime * MovementSpeed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= transform.forward * Time.deltaTime * MovementSpeed;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position -= transform.right * Time.deltaTime * MovementSpeed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += transform.right * Time.deltaTime * MovementSpeed;
+        }
+        Gholam.transform.position = transform.position + offset;
+    }
+}
---
+++ Assets/Player_mov2.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_mov2 : MonoBehaviour
+{
+    Vector3 offset;
+    public Camera Gholam;
+    Rigidbody MyRigidBody;
+    // Use this for initialization
+    void Start()
+    {
+        offset = Gholam.transform.position - transform.position;
+        MyRigidBody = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            MyRigidBody.AddForce(0, 0, 10);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            MyRigidBody.AddForce(0, 0, -10);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            MyRigidBody.AddForce(-10, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            MyRigidBody.AddForce(10, 0, 0);
+        }
+        Gholam.transform.position = transform.position + offset;
+    }
+}
